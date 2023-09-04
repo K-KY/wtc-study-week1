8bbb6a41ff4790b9493d8789c0320cbe0157c40a
K-KY <kyuyoungk@naver.com>
2023-09-04 09:51:21 +0900

    feat : 0 부터 값을 증가시키며 같은 값이 있는지 확인한다.

--- src/main/java/onboarding/problem2/SlidingWindow.java
+++ src/main/java/onboarding/problem2/SlidingWindow.java
@@ -1,8 +1,17 @@
 package onboarding.problem2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SlidingWindow {
+    public List<String> sliding(String[] psw) {
+        List<String> result =new ArrayList<>(Arrays.asList(psw));
+        for (int i = 0; i < result.size(); i++) {
+            i = compare(i, result);
+        }
+        return result;
+    }
     public int compare(int i, List<String> psw) {
         int index = i;
         if (i < psw.size() - 1 && psw.get(i).equals(psw.get(i + 1))) {
