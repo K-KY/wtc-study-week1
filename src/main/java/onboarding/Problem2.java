4005a5ac30b8a0cdae5828a15a40532cd925467b
K-KY <kyuyoungk@naver.com>
2023-09-04 10:09:14 +0900

    feat : 결과를 리턴

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,11 @@
 package onboarding;
 
+import onboarding.problem2.SlidingWindow;
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer;
+        String result =  new SlidingWindow().sliding(cryptogram.split(""));
+        answer = result.substring(1, result.length() - 1);
         return answer;
     }
 }
