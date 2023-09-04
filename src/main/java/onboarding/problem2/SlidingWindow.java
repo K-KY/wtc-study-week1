b4efd69ce089a9633a6c4da7adb49c38a0e54b3b
K-KY <kyuyoungk@naver.com>
2023-09-04 09:48:27 +0900

    fix : compare 메소드에서 리스트 변경 시 i 를 바뀐 인덱스만큼 뺀다

--- src/main/java/onboarding/problem2/SlidingWindow.java
+++ src/main/java/onboarding/problem2/SlidingWindow.java
@@ -3,15 +3,18 @@ package onboarding.problem2;
 import java.util.List;
 
 public class SlidingWindow {
-    public List<String> compare(int i, List<String> psw) {
-        if (i < psw.size() && psw.get(i).equals(psw.get(i + 1))) {
+    public int compare(int i, List<String> psw) {
+        int index = i;
+        if (i < psw.size() - 1 && psw.get(i).equals(psw.get(i + 1))) {
             psw.remove(i);
             psw.remove(i);
+            index -= 1;
         }
         if (i < psw.size() && i > 0 && psw.get(i).equals(psw.get(i - 1))) {
             psw.remove(i);
             psw.remove(i - 1);
+            index -= 2;
         }
-        return psw;
+        return index;
     }
 }
