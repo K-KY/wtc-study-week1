c7e43af5ded9a11d37ba5ebc791d70b1f8bc3ae6
K-KY <kyuyoungk@naver.com>
2023-09-05 09:57:18 +0900

    feat : 리스트의 값이 3의 배수 이면 카운팅

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
     }
+    static int counter(List<String> lst) {
+        int cnt = 0;
+        for (int i = 0; i < lst.size(); i++) {
+            int n = Integer.parseInt(lst.get(i));
+            if (n % 3 == 0) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
 }
