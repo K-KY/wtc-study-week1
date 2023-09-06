23ccca7fd43df2efbf546c2e19907e1dc01f4e9d
K-KY <kyuyoungk@naver.com>
2023-09-06 14:15:18 +0900

    refactor : 메소드 간소화

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,25 +3,28 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        System.out.println(frogging(word));
+        answer = frogging(word);
         return answer;
     }
     static String frogging(String str) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) >= 'Z') {
-                sb.append(frogLower(str.charAt(i)));
-                continue;
-            }
-            sb.append(frogUpper(str.charAt(i)));
+            char c = str.charAt(i);
+            sb.append(frog(c));
         }
         return sb.toString();
     }
-    static char frogLower(char c) {
-        return (char) ('a' + ('z' - c));
+    static char frog(char c) {
+        if (c >= 'a' && c <= 'z' ) {
+            return (char) ('a' + ('z' - c));
+        }
+        return frogUpper(c);
     }
     static char frogUpper(char c) {
-        return (char)('A' + ('Z' - c));
+        if (c >= 'A' && c <= 'Z' ) {
+            return (char)('A' + ('Z' - c));
+        }
+        return c;
     }
 }
 //A = 65 Z = 90
