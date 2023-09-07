4670bd7b3d1918baad2c0f98de73b76db10ff8da
K-KY <kyuyoungk@naver.com>
2023-09-07 16:29:14 +0900

    feat : 결과를 리턴한다

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,16 @@
 package onboarding;
 
+import onboarding.problem6.Similarity;
+
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        Similarity s = new Similarity();
+        answer = s.similarityCheck(forms);
+        Collections.sort(answer);
         return answer;
     }
 }
