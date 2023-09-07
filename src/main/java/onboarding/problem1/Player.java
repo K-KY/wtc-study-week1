bf514aa2a1abe151a3d95cc00b55b7fe6161f4cd
K-KY <kyuyoungk@naver.com>
2023-09-07 11:52:02 +0900

    fix : Problem1.Player 접근 지정자 변경

--- src/main/java/onboarding/problem1/Player.java
+++ src/main/java/onboarding/problem1/Player.java
@@ -4,31 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-    List<Integer> page = new ArrayList<>();
-    int score = 0;
+    private List<Integer> page = new ArrayList<>();
+    private int score = 0;
     int id = 0;
 
-    public void setId(int id) {
+    protected void setId(int id) {
         this.id = id;
     }
 
-    public void setScore(int score) {
+    protected void setScore(int score) {
         this.score = score;
     }
 
-    public int getScore() {
+    protected int getScore() {
         return score;
     }
 
-    public void setPage(List<Integer> page) {
+    protected void setPage(List<Integer> page) {
         this.page = page;
     }
 
-    public int getId() {
+    protected int getId() {
         return id;
     }
 
-    public List<Integer> getPage() {
+    protected List<Integer> getPage() {
         return page;
     }
 }
