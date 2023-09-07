d56ed95158a196b992292741494d87a871bc23e3
K-KY <kyuyoungk@naver.com>
2023-09-07 16:27:57 +0900

    feat : compare에 파라미터를 전달한다

--- src/main/java/onboarding/problem6/Similarity.java
+++ src/main/java/onboarding/problem6/Similarity.java
@@ -7,6 +7,14 @@ import java.util.List;
 public class Similarity {
     List<String> emails = new ArrayList<>();
 
+    private void finder(List<List<String>> form, String[] windows) {
+        for (int i = 0; i < form.size(); i++) {
+            String crewEmail = form.get(i).get(0);
+            String crewName = form.get(i).get(1);
+            compare(windows, crewEmail, crewName);
+        }
+    }
+
     private void compare(String[] windows, String crewEmail, String crewName) {
         if (Arrays.equals(windows, new SlidingWindow().window(crewName))) {
             return;
