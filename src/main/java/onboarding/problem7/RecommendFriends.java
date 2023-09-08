97c2946933879014fe21e614911fb679ba59a05c
K-KY <kyuyoungk@naver.com>
2023-09-09 01:24:05 +0900

    comment : Problem7 주석

--- src/main/java/onboarding/problem7/RecommendFriends.java
+++ src/main/java/onboarding/problem7/RecommendFriends.java
@@ -16,9 +16,11 @@ public class RecommendFriends {
         for (int i = 0; i < visitor.size(); i++) {
             String key = visitor.get(i);
             if (friendsMap.get(key) == null) {
+                //visitor가 친구가 아닐때
                 isNotFriend(key, friendsMap);
             }
             if (friendsMap.get(key) != null) {
+                //visitor가 친구일 때
                 isFriend(friendsMap.get(key));
             }
         }
@@ -27,18 +29,22 @@ public class RecommendFriends {
     }
 
     private void isNotFriend(String key, HashMap<String, List<String>> friendsMap) {
+        //visitort가 친구가 아닐 때 친구의 친구인지 확인한다
         boolean[] booleans ={false};
         friendsMap.forEach((k, v) -> {
+            //친구의 친구면 + 10점
             if (v.contains(key)) {
                 score.put(key, score.getOrDefault(key, 0) + 10);
                 booleans[0] = true;
             }
         });
+        //boolean이 false 면 친구의 친구도 아님 + 1점
         if (!booleans[0]) {
             score.put(key, score.getOrDefault(key, 0) + 1);
         }
     }
     private void isFriend(List<String> friends) {
+        //친구면 리스트에 있는 모두 +10점
         friends.forEach(v -> {
             score.put(v, score.getOrDefault(v, 0) + 10);
         });
@@ -46,8 +52,9 @@ public class RecommendFriends {
 
     public List<String> sortKey() {
         List<String> keySet = new ArrayList<>(score.keySet());
-        keySet.sort((o1, o2) -> score.get(o2).compareTo(score.get(o1)));
-        System.out.println(keySet);
+        //score의 keySet을 저장한다
+            keySet.sort((o1, o2) -> score.get(o2).compareTo(score.get(o1)));
+            //keySey을 value를 기준으로 내림차순 정렬한다 o2 > o1 이면 둘이 바꿈
         return keySet;
     }
 }
--- src/main/java/onboarding/problem7/UserFriends.java
+++ src/main/java/onboarding/problem7/UserFriends.java
@@ -10,6 +10,7 @@ public class UserFriends {
         for (int i = 0; i < friends.size(); i++) {
             String friend = friends.get(i).get(1);
             if (friend.equals(user)) {
+                //user와 친구인 사람을 찾는다
                 makeFriendList(friends.get(i).get(0), friends);
             }
         }
@@ -22,6 +23,7 @@ public class UserFriends {
                 List<String> friendList = friendMap.getOrDefault(friend, new ArrayList<>());
                 friendList.add(friends.get(i).get(1));
                 friendMap.put(friend, friendList);
+                //friend와 같으면 Map에 넣는다
             }
         }
     }
