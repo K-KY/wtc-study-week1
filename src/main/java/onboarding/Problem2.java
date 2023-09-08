e460753d95718b14b54914126274b791b856c772
K-KY <kyuyoungk@naver.com>
2023-09-08 23:29:34 +0900

    comment : Problem2 주석

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,8 +4,10 @@ import onboarding.problem2.SlidingWindow;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer;
-        String result =  new SlidingWindow().sliding(cryptogram.split(""));
+        String result =  new SlidingWindow().sliding(cryptogram.split(""));//cryptogram 을 글자별로 분리해서 파라미터로 전달
         answer = result.substring(1, result.length() - 1);
+        //리턴 받을때 ", " 를 제거해서 리턴 했으니까 [abcdefg]가 저장되어있음
+        //양 끝의 브라켓의 index가 0, length() 까지니까 이 사이의 값만 뽑아서 저장한다
         return answer;
     }
 }
--- src/main/java/onboarding/problem2/SlidingWindow.java
+++ src/main/java/onboarding/problem2/SlidingWindow.java
@@ -6,22 +6,35 @@ import java.util.List;
 
 public class SlidingWindow {
     public String sliding(String[] psw) {
-        List<String> result =new ArrayList<>(Arrays.asList(psw));
+
+        //글자별로 나눠진 cryptogram 을 리스트에 넣는다
+        List<String> result = new ArrayList<>(Arrays.asList(psw));
         for (int i = 0; i < result.size(); i++) {
+            //i 와 result 를 같이 파라미터로 넣는다
+            //i를 넣는건 result에서 삭제된 만큼 i 를 빼주기 위함
             i = compare(i, result);
         }
+        //list를 그대로 String으로 바꿔서 리턴한다.  처음 값은 [a, b, c, d] 이므로 ", " 를 제거후 리턴한다
         return result.toString().replace(", ", "");
     }
+
     public int compare(int i, List<String> psw) {
         int index = i;
+        //앞의 값과 같은경우
         if (i < psw.size() - 1 && psw.get(i).equals(psw.get(i + 1))) {
             psw.remove(i);
+            //i 를 빼고 나면 i + 1 은 i 가 됨
             psw.remove(i);
+            //[a, b, {c, d,} e, f, g]
+            //i = e 가 됨 그럼 e 는 건너 뛰고 f부터 비교를 하니까 e 부터 시작하기 위해 -1
             index -= 1;
         }
+        //뒤의 값과 같은경우
         if (i < psw.size() && i > 0 && psw.get(i).equals(psw.get(i - 1))) {
             psw.remove(i);
             psw.remove(i - 1);
+            //[a, b, {c, d,} e, f, g]
+            //i = f 가 됨 그럼 바로 g 로 가니까 e 부터 비교하기 위해 -2
             index -= 2;
         }
         return index;
