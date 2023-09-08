f58836672952daebfba8604f933245c96ff9f6a4
K-KY <kyuyoungk@naver.com>
2023-09-09 00:59:36 +0900

    comment : Problem6 주석

--- src/main/java/onboarding/problem6/Similarity.java
+++ src/main/java/onboarding/problem6/Similarity.java
@@ -10,6 +10,8 @@ public class Similarity {
         for (int i = 0; i < forms.size(); i++) {
             String crewName = forms.get(i).get(1);
             String[] windows = new SlidingWindow().window(crewName);
+            //이름을 두글자씩 나눠서 저장한다 -> 가나다 = 가나, 나다
+
             finder(forms, windows);
         }
         return emails;
@@ -25,11 +27,14 @@ public class Similarity {
 
     private void compare(String[] windows, String crewEmail, String crewName) {
         if (Arrays.equals(windows, new SlidingWindow().window(crewName))) {
+            //이게 둘이 같으면 같은 사람이니까 그냥 리턴
             return;
         }
         for (int i = 0; i < windows.length; i++) {
+            //만약 windows 중 하나라도 같으면 중복이니까 emails 에 추가, 중복값으 들어가지 않도록 조건 넣음
             if (crewName.contains(windows[i]) && !emails.contains(crewEmail)) {
                 emails.add(crewEmail);
+                break;
             }
 
         }
--- src/main/java/onboarding/problem6/SlidingWindow.java
+++ src/main/java/onboarding/problem6/SlidingWindow.java
@@ -2,9 +2,11 @@ package onboarding.problem6;
 public class SlidingWindow {
     public String[] window(String name) {
         String[] windows = new String[name.length() - 1];
+        //이름이 10글자면 2글자로 나눠진 갯수는 9개
         for (int i = 0; i < name.length() -1; i++) {
             windows[i] = String.valueOf(name.charAt(i)) + name.charAt(i + 1);
         }
+
         return windows;
     }
 }
