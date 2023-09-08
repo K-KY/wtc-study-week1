d061c2e8123ceb45f587b2969300a7527ec1b453
K-KY <kyuyoungk@naver.com>
2023-09-08 15:10:24 +0900

    feat : user의 친구와 친구인 리스트를 리턴한다

---
+++ src/main/java/onboarding/problem7/UserFriends.java
@@ -0,0 +1,19 @@
+package onboarding.problem7;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class UserFriends {
+    HashMap<String, List<String>> friendMap = new HashMap<>();
+
+    private void makeFriendList(String friend, List<List<String>> friends) {
+        for (int i = 0; i < friends.size(); i++) {
+            if (friends.get(i).get(0).equals(friend)) {
+                List<String> friendList = friendMap.getOrDefault(friend, new ArrayList<>());
+                friendList.add(friends.get(i).get(1));
+                friendMap.put(friend, friendList);
+            }
+        }
+    }
+}
