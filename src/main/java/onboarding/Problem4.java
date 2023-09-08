ad24471fb0c61d05928444a69b8aed634be03eea
K-KY <kyuyoungk@naver.com>
2023-09-08 23:49:27 +0900

    comment : Problem4 주석

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,7 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer;
         answer = frogging(word);
         return answer;
     }
@@ -10,14 +10,20 @@ public class Problem4 {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
+            //str 의 i번째 글자를 잘라서 c에 넣음
             sb.append(frog(c));
+            //frog의 리턴값을 스트링빌더에 넣음
         }
         return sb.toString();
     }
     static char frog(char c) {
         if (c >= 'a' && c <= 'z' ) {
+            //c 가 'b' 일때 개구리 사전에서는 'y'
+            //'z' - c = 122 - 98 = 24
+            //'a' + 24 = 97 + 24 = 121
             return (char) ('a' + ('z' - c));
         }
+        //if 문 에서 리턴을 안했으면 알파벳이 아니거나 대문자일 경우임
         return frogUpper(c);
     }
     static char frogUpper(char c) {
